1fd44aaf7576e6dd1656c290e23b11a3f480b399
Amkam <amaduskamara@gmail.com>
2022-10-30 21:40:47 +0000

    Add background to the App component's main

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 const App = () => {
   return (
-    <div className="App">
+    <main className="antialiased text-slate-500 dark:text-slate-400 bg-white dark:bg-slate-900 h-screen">
       <h1>React Card Test</h1>
-    </div>
+    </main>
   );
 };
 
