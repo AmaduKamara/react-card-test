3d5d384074fbc833f846e0e607acf0c25741bdd2
Amkam <amaduskamara@gmail.com>
2022-10-31 11:16:16 +0000

    Style footer and add footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t-2 border-gray-400 w-full py-4 mt-6 absolute bottom-0 px-16">
+      <p>footer</p>
+    </footer>
+  );
 };
 
 export default Footer;
