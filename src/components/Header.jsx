43ffd06eb03614dd174c6c6125144726cede88c0
Amkam <amaduskamara@gmail.com>
2022-10-31 11:16:31 +0000

    Style header and add buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="py-4 border-b-2 border-gray-400 absolute top-0 left-0 w-full px-16">
+      <button className="font-semibold">Add Card</button>
+      <button className="font-semibold ml-10">Sort Card</button>
+    </header>
+  );
 };
 
 export default Header;
