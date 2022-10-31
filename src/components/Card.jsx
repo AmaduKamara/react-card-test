ecee77a0be8a801edb2df87039e64ef64090df20
Amkam <amaduskamara@gmail.com>
2022-10-31 11:15:54 +0000

    Create card structure and design

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { MdOutlineClose } from "react-icons/md";
 
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className="border border-gray-200 p-16 flex justify-center items-center relative">
+      <button>
+        <MdOutlineClose className="text-gray-200 absolute top-0 right-0 mt-2 mr-3" />
+      </button>
+      <p>74</p>
+    </div>
+  );
 };
 
 export default Card;
