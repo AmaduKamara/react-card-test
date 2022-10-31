47df8b5bfde8fd2f82f46b7de5dbf01e904d9143
Amkam <amaduskamara@gmail.com>
2022-10-31 11:16:55 +0000

    Style instructions component

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Instructions = () => {
-  return <div>Instructions</div>;
+  return (
+    <aside className="border-l-2 w-[400px] h-screen flex justify-center items-center">
+      <h2 className="text-white text-2xl">Instructions</h2>
+    </aside>
+  );
 };
 
 export default Instructions;
