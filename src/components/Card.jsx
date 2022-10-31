53fe912c10574738552326d357dd323f3fac89db
Amkam <amaduskamara@gmail.com>
2022-10-31 13:46:55 +0000

    Add height to cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import { MdOutlineClose } from "react-icons/md";
 
 const Card = () => {
   return (
-    <div className="border border-gray-200 p-16 flex justify-center items-center relative">
+    <div className="border border-gray-200 p-16 flex justify-center items-center relative h-32">
       <button>
         <MdOutlineClose className="text-gray-200 absolute top-0 right-0 mt-2 mr-3" />
       </button>
