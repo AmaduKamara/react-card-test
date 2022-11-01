196e610a22863c5d05aeb897dff1d6a0a67fb96f
Amkam <amaduskamara@gmail.com>
2022-11-01 10:59:55 +0000

    Receive props and pass to card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { MdOutlineClose } from "react-icons/md";
 
-const Card = () => {
+const Card = ({ card, handleDeleteCard }) => {
   return (
-    <div className="border border-gray-200 p-16 flex justify-center items-center relative h-32">
+    <div className="border border-gray-200 p-16 flex justify-center items-center relative max-h-32 transition duration-500">
       <button>
-        <MdOutlineClose className="text-gray-200 absolute top-0 right-0 mt-2 mr-3" />
+        <MdOutlineClose
+          className="text-gray-200 absolute top-0 right-0 mt-2 mr-3"
+          onClick={() => handleDeleteCard(card.value)}
+        />
       </button>
-      <p>74</p>
+      <p>{card.value}</p>
     </div>
   );
 };
