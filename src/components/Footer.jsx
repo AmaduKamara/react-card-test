d3566c3318eea2b0588c877f1fc09526924b4364
Amkam <amaduskamara@gmail.com>
2022-11-01 11:00:15 +0000

    Add z-index to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="border-t-2 border-gray-400 w-full py-4 mt-6 absolute bottom-0 px-16">
+    <footer className="border-t-2 border-gray-400 w-full py-4 mt-6 absolute bottom-0 px-16 z-50">
       <p>footer</p>
     </footer>
   );
